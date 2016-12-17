93fcdfbfd496991401c647356278ab3b201a6dbd
ukroliki <ukroliki@outlook.com>
2016-12-17 07:30:37 +0700

    quiz exist

--- quiz/quiz/Program.cs
+++ quiz/quiz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,24 @@ namespace quiz
         static int Score = 0;
         static int queezNum = 0;
         static string[] quest;
+        public static int Input()
+        {
+            if (Directory.Exists(@".\quest"))
+            {
+                int n = Directory.GetFiles(@".\quest").Length;
+                if (n == 0)
+                {
+                    Console.WriteLine("Ошибка! Нет вопросов!");
+                    return 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка, Вопросы не найдены!");
+                return 2;
+            }
+            return 0;
+        }
         static void Main(string[] args)
         {
         }

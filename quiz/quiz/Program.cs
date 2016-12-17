4fdb391cb1999fa7e6e768d71f808e723c76a5aa
ukroliki <ukroliki@outlook.com>
2016-12-17 07:26:10 +0700

    Init vars

--- quiz/quiz/Program.cs
+++ quiz/quiz/Program.cs
@@ -8,6 +8,11 @@ namespace quiz
 {
     class Program
     {
+        static Random rand = new Random();
+        static string Name = "";
+        static int Score = 0;
+        static int queezNum = 0;
+        static string[] quest;
         static void Main(string[] args)
         {
         }
